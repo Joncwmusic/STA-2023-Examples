68497c9d7c05b0a0c55ee37417be57d1c3c85af9
Jon Whelpley <jon.chris.whelpley@gmail.com>
2026-01-21 17:15:17 -0500

    Skeleton Chapter 3 Notes

--- Chapter 3 Files/build_central_tendency.py
+++ Chapter 3 Files/build_central_tendency.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-def get_average(numlist):
+def get_arithmetic_mean(numlist):
     return sum(numlist)/len(numlist)
 
 
@@ -14,9 +14,14 @@ def get_median(numlist):
 
 
 def build_central_tendency_tab(user_string):
+    example_data = [4, 7, 10, 12, 15, 17, 19, 20, 21, 25]
+
     st.title("Measures of Centeral Tendency")
-    st.header("Mean (Average)")
-    st.text("")
+    st.header("Arithmetic Mean (Average)")
+    st.text("The arithmetic mean is a simple sum of all the data points after which you just have to divide that "
+            "sum by the number of data points to get your final number. Regardless of if you're measuring the mean "
+            "of a sample or a population, it is calculated the same way.")
+
 
     st.header("Median")
 
@@ -24,8 +29,18 @@ def build_central_tendency_tab(user_string):
 
     st.header("Now You Try")
     st.text("input your dataset here (make sure to separate each number with a comma:")
-    st.text_input()
+    user_string = st.text_input()
+    user_data_list = list(map(int, user_string.split(',')))
 
-    return None
+    st.button("Reset", type="primary")
+    if st.button("Compute Mean"):
+        user_mean = get_arithmetic_mean(user_data_list)
+        st.write(str(user_mean))
+    else:
+        st.write("Goodbye")
+
+    if st.button("Aloha", type="tertiary"):
+        st.write("Ciao")
+
 
 user_string = "3, 5, 6, 10, 12, 16, 18"
