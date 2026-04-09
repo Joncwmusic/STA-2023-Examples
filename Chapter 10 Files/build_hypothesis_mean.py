6b5617b44f26fd4f1378f9f8360d0bbe799efe63
Jon Whelpley <jon.chris.whelpley@gmail.com>
2026-04-08 23:13:00 -0400

    Mean HTest is pretty much done might need a few tweaks and left is calculator and props

--- Chapter 10 Files/build_hypothesis_mean.py
+++ Chapter 10 Files/build_hypothesis_mean.py
@@ -1,2 +1,147 @@
+import math
+import random
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objs as go
+
+def factorial(n):
+    product = 1
+    for i in range(n):
+        product *= i + 1
+    return product
+
+def binompdf(n, p, x):
+    prob = factorial(n)/(factorial(n-x)*factorial(x)) * p**x*(1-p)**(n-x)
+    return prob
+
+def binomcdf(n, p, x):
+    sum = 0
+    for i in range(x+1):
+        sum += binompdf(n, p, i)
+    return sum
+
+def normpdf(mu, sigma, x):
+    part_1 = 1/(sigma*math.sqrt(2*math.pi))
+    part_2 = math.exp(-((x-mu)**2)/(2*sigma**2))
+    return part_1 * part_2
+
 def build_hypothesis_mean():
-    pass
+
+    salary_data = [84200, 64800, 72000, 104250, 109000, 68500, 125000, 69900, 99800, 87500, 78900, 85000, 36500, 127000]
+
+    salary_mean = sum(salary_data)/len(salary_data)
+    salary_deviations = [(i - salary_mean)**2 for i in salary_data]
+    salary_ssd = math.sqrt(sum(salary_deviations)/(len(salary_deviations)-1))
+    test_stat = (salary_mean - 64700)/(salary_ssd/math.sqrt(14))
+
+
+    # normal line data
+    x_values_full = [i/50 for i in range(-200, 200)]
+    x_values_left = [i / 50 for i in range(-200, -50)]
+    x_values_right = [i / 50 for i in range(125, 200)]
+
+    normal_full_line = [normpdf(mu=0, sigma=1, x=i) for i in x_values_full]
+    normal_left_area = [normpdf(mu=0, sigma=1, x=i) for i in x_values_left]
+    normal_right_area = [normpdf(mu=0, sigma=1, x=i) for i in x_values_right]
+    normal_both_area = [normpdf(mu=0, sigma=1, x=i) if (i > 2 or i < -2) else 0 for i in x_values_full ]
+
+    normal_full_line_df = pd.DataFrame({"x_val":x_values_full,"y_val":normal_full_line})
+    normal_left_area_df = pd.DataFrame({"x_val": x_values_left, "y_val": normal_left_area})
+    normal_right_area_df = pd.DataFrame({"x_val": x_values_right, "y_val": normal_right_area})
+    normal_both_tail_df = pd.DataFrame({"x_val": x_values_full, "y_val": normal_both_area})
+
+    normal_line_fig_right = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_right.add_scatter(x=normal_right_area_df['x_val']
+                                     , y=normal_right_area_df['y_val']
+                                     , fill='tozeroy', showlegend=False)
+    normal_line_fig_right.add_vline(x=3.3, line_width=2, line_dash="dash", line_color="green")
+    normal_line_fig_left = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_left.add_scatter(x=normal_left_area_df['x_val']
+                                , y=normal_left_area_df['y_val']
+                                , fill='tozeroy', showlegend=False)
+    normal_line_fig_both = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_both.add_scatter(x=normal_both_tail_df['x_val']
+                                     , y=normal_both_tail_df['y_val']
+                                     , fill='tozeroy', showlegend=False)
+
+    st.header("Hypothesis Testing for a Mean")
+    st.markdown("When testing a hypothesis on the population mean, the basic idea is assuming the null hypothesis $H_0$"
+                " is true, then gathering a sample and finding a sample mean. The rest is determining how likely "
+                "getting the mean for you sample is if the null is in fact true. If that probability is low (p value), "
+                "or the z score for your sample mean is beyond a certain value (classical method) you can say 'hmm... "
+                "it seems impossibly unlikely I would get this sample average from the population, maybe I should "
+                "challenge my assumption and reject the $H_0$'")
+
+    st.markdown("Let's start with an example. Suppose you're arguing with your boss asserting that you are underpaid "
+                "and are *this* close to becoming a proletariat hell bent on overthrowing your capitalist oppressor. "
+                "Your boss; however, says your wage is the market rate. So you go to LinkedIn and message a random "
+                "sample of people with our same job title in your industry and intrusively ask how much they make.")
+
+    st.dataframe(salary_data)
+    st.markdown("**The Average Salary**: " + str(round(salary_mean,2)))
+    st.markdown("**Your Salary**: 65700... oof")
+    st.markdown("You bring this data to your boss and he says 'that sample is random and you could've easily gotten "
+                "any other average that would be below your salary.' This is a fair point, but you, stubborn, and "
+                "feeling incredibly dejected from the remark, now need to convince you boss that the chance your sample"
+                " overestimates to the degree by which it does is so unlikely given his other statement that you are "
+                "paid MARKET rate. This is where the hypothesis test comes in.")
+
+    st.header("Setting Up the Hypothesis Test")
+    st.markdown("First you must determine your hypothesis setup. Your boss has claimed your salary is the average. But "
+                "you're claiming instead that the average is higher that your salary. This mean your null and "
+                "alternative look like this: ")
+    st.markdown(r"$H_0: \mu = 65700$")
+    st.markdown(r"$H_1: \mu > 65700$")
+
+    st.markdown("For the sake of removing doubt let's say we're operating on 99% confidence or a significance level of "
+                "0.01.")
+
+    st.markdown("Lastly, let's get the point estimates for the mean and standard deviation:")
+    st.markdown(r"$\bar{x} = " + str(round(salary_mean,2)) + "$")
+    st.markdown(r"$s_x = " + str(round(salary_ssd,2))+ "$")
+
+    st.header("Getting a Test Statistic")
+
+    st.markdown("The test statistic for the mean is as follows:")
+    st.markdown(r"$t_0 = \dfrac{\bar{x} - \mu}{\dfrac{s}{\sqrt{n}}}$")
+    st.markdown("This may look familiar as it is basically the z score of the sample mean. Now let's plug in all of "
+                "our values:")
+    st.markdown(r"$t_0 = \dfrac{\bar{x} - \mu}{\dfrac{s}{\sqrt{n}}} \Rightarrow \dfrac{86596.43 - 65700}"
+                r"{\dfrac{24756}{\sqrt{14}}} = " + str(round(test_stat,4)) + "$")
+
+    st.header("Classical Approach")
+
+    st.markdown(r"When using the classical approach you need to compare your test statistic to the critical value. You "
+                r"can find a critical value by using `invT(area, df)` where the area is your alpha and df is the "
+                r"degrees of freedom which is your sample size minus 1. For a left tail test use area = alpha, right "
+                r"tail use area = 1-alpha, and for a 2 tail test use alpha/2 and take both the positive and negative "
+                r"values.")
+
+    st.markdown("If you aren't using a computer to do this for you, you can use a t table with common alpha values and "
+                "estimate for various degrees of freedom. The critical value for a right tail test with alpha = 0.01 "
+                "and degrees of freedom of 13 is ~2.65")
+
+    st.plotly_chart(normal_line_fig_right, key = "right tail t")
+
+    st.markdown("Now all you have to do is compare the test statistic to the critical value. Turns out 3.3 is in fact "
+                "bigger than 2.65, so the assumption that you pay is market rate is likely wrong and you can tell your "
+                "boss to shove it before he fires you. But you can hold your head high knowing you are right.")
+
+    st.header("P Value Approach")
+
+    st.markdown("The p value approach is a little different. It asks the question 'how likely is it to get your average"
+                " or worse given your assumed average?' You still need the test statistic but now you compute "
+                "$Pr(t>3.31)$ which is... low (0.0028). Since it's lower than 0.01, it seems like your boss is "
+                "doubly wrong.")
+
+    st.header("In Summary:")
+    st.markdown(r"""
+    * Define your hypothesis type and get your variables
+    * Get a significance level $\alpha$ or a critical value
+    * Get a test statistic based on your data
+    * Compare:
+        * Compare the test statistic to your critical value OR
+        * Compare the p value of the test statistic to the significance level
+    """)
--- Chapter 10 Files/build_hypothesis_prop.py
+++ Chapter 10 Files/build_hypothesis_prop.py
@@ -1,2 +1,178 @@
+import math
+import random
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objs as go
+
+def factorial(n):
+    product = 1
+    for i in range(n):
+        product *= i + 1
+    return product
+
+def binompdf(n, p, x):
+    prob = factorial(n)/(factorial(n-x)*factorial(x)) * p**x*(1-p)**(n-x)
+    return prob
+
+def binomcdf(n, p, x):
+    sum = 0
+    for i in range(x+1):
+        sum += binompdf(n, p, i)
+    return sum
+
+def normpdf(mu, sigma, x):
+    part_1 = 1/(sigma*math.sqrt(2*math.pi))
+    part_2 = math.exp(-((x-mu)**2)/(2*sigma**2))
+    return part_1 * part_2
+
 def build_hypothesis_prop():
-    pass
+    hypothesis_test_outcomes = {
+        "Decision": ["Reject Null","Do Not Reject Null"],
+        "Null is True": ["Type I Error", "Correct"],
+        "Null is Not True": ["Correct", "Type II Error"],
+    }
+
+    # normal line data
+    x_values_full = [i/50 for i in range(-200, 200)]
+    x_values_left = [i / 50 for i in range(-200, -50)]
+    x_values_right = [i / 50 for i in range(50, 200)]
+
+    normal_full_line = [normpdf(mu=0, sigma=1, x=i) for i in x_values_full]
+    normal_left_area = [normpdf(mu=0, sigma=1, x=i) for i in x_values_left]
+    normal_right_area = [normpdf(mu=0, sigma=1, x=i) for i in x_values_right]
+    normal_both_area = [normpdf(mu=0, sigma=1, x=i) if (i > 2 or i < -2) else 0 for i in x_values_full ]
+
+    normal_full_line_df = pd.DataFrame({"x_val":x_values_full,"y_val":normal_full_line})
+    normal_left_area_df = pd.DataFrame({"x_val": x_values_left, "y_val": normal_left_area})
+    normal_right_area_df = pd.DataFrame({"x_val": x_values_right, "y_val": normal_right_area})
+    normal_both_tail_df = pd.DataFrame({"x_val": x_values_full, "y_val": normal_both_area})
+
+    normal_line_fig_right = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_right.add_scatter(x=normal_right_area_df['x_val']
+                                     , y=normal_right_area_df['y_val']
+                                     , fill='tozeroy', showlegend=False)
+    normal_line_fig_left = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_left.add_scatter(x=normal_left_area_df['x_val']
+                                , y=normal_left_area_df['y_val']
+                                , fill='tozeroy', showlegend=False)
+    normal_line_fig_both = px.line(data_frame=normal_full_line_df, x="x_val", y="y_val")
+    normal_line_fig_both.add_scatter(x=normal_both_tail_df['x_val']
+                                     , y=normal_both_tail_df['y_val']
+                                     , fill='tozeroy', showlegend=False)
+
+    st.header("What is a Hypothesis and How Do We Test It?")
+    st.markdown("A Hypothesis is a statement about a parameter of a population. Some examples include: ")
+    st.markdown(r"""
+    * The average height of all students on a college campus is 68 inches.
+    * The proportion of college educated students that want stricter gun laws is 58%
+    * The average home price in Florida is 439000
+    """)
+    st.markdown("Now you could believe these statements and take them at face value but you're a skeptical researcher "
+                "who wants to verify these facts. The problem is you have no money or resources to collect all of this "
+                "information. What are you to do without the finances to ask every homeowner in florida to get the true"
+                "average?")
+    st.markdown("You *CAN* get a sample. But it won't be perfect. So we then have to *INFER* based on what we collect "
+                "and define a threshold to determine if we should accept the statistical statement or not. Those "
+                "thresholds are based on your sample size and more or less the likelihood of you just getting a bad "
+                "sample if we assume the original statement is true.")
+
+    st.subheader("The Type of Hypothesis Tests")
+    st.markdown("Every hypothesis is framed around 2 statements. The first is the null hypothesis $H_0$. This is the "
+                "statement you're trying to challenge. You assume this statement is true before collecting and "
+                "analyzing your sample. The second is the alternative hypothesis to challenge the original claim $H_1$."
+                " This will always challenge the null hypothesis depending on the context.")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.markdown("2 Tail Tests")
+        st.markdown(r"$H_0: \mu = \mu_0$")
+        st.markdown(r"$H_1: \mu \neq \mu_0$")
+        st.plotly_chart(normal_line_fig_both)
+    with col2:
+        st.markdown("Left Tail Tests")
+        st.markdown(r"$H_0: \mu = \mu_0$")
+        st.markdown(r"$H_1: \mu < \mu_0$")
+        st.plotly_chart(normal_line_fig_left)
+    with col3:
+        st.markdown("Right Tail Tests")
+        st.markdown(r"$H_0: \mu = \mu_0$")
+        st.markdown(r"$H_1: \mu > \mu_0$")
+        st.plotly_chart(normal_line_fig_right)
+
+    st.header("Type I rand Type II Errors")
+    st.markdown("When determining the outcome of a hypothesis test we have 4 options for how to conclude the test:")
+
+    st.table(hypothesis_test_outcomes)
+
+    st.markdown(r"""* We can be correct, which is when we reject the null hypothesis when it is false OR when we don't 
+        reject the null when it is true.
+        * Meanwhile if we reject a true null hypothesis then we have a Type I Error
+        * Likewise if we fail to reject a false null hypothesis we have a Type II Error
+    """)
+
+    st.markdown(r"As it turns out this $\alpha$ value is just the probability of a Type I Error. In other words the "
+                r"significance level is the probability of rejecting a true $H_0$")
+    st.markdown(r"$\alpha = Pr(\text{Type I Error})$")
+
+    st.markdown(r"""
+       One Tail Critical Value Z Scores:
+
+       - 90% Confidence $z_{\alpha} = z_{0.1} = 1.28$
+       - 95% Confidence $z_{\alpha} = z_{0.05} = 1.645$
+       - 99% Confidence $z_{\alpha} = z_{0.01} = 2.32$
+
+       Two Tail Critical Value Z Scores:
+
+       - 90% Confidence $z_{\alpha / 2} = z_{0.05} = 1.645$
+       - 95% Confidence $z_{\alpha / 2} = z_{0.025} = 1.96$
+       - 99% Confidence $z_{\alpha / 2} = z_{0.005} = 2.576$
+       """)
+
+    st.header("P Value Approach")
+
+    st.markdown(r"$t_0 = \dfrac{\bar{x} - \mu}{\dfrac{s}{\sqrt{n}}}$")
+
+    st.markdown(r"$Pr(t > t_0)$")
+    st.markdown(r"$Pr(t > t_0)$")
+    st.markdown(r"$Pr(t > |t_0|)$")
+
+    st.markdown(r"""
+       Critical Value P Values:
+
+       - 90% Confidence $\alpha = 0.1$
+       - 95% Confidence $\alpha = 0.05$
+       - 99% Confidence $\alpha = 0.01$
+       """)
+
+    st.header("Outlining the Methods:")
+
+    st.subheader("Method 1: The Classical Approach")
+
+    st.markdown(r"""
+        * Identify the null and alternative hypotheses and significance level (default to 0.05 if not given in a problem)
+        * Compute the test statistic $z_0$ i.e. the z score for the sample mean or proportion
+        * Compute $z_\alpha$ AKA the critical value to compare the test statistic to determine if you should reject $H_0$
+        """)
+
+    st.subheader("Method 2: The P Value Approach")
+
+    st.markdown(r"""
+        * Identify the null and alternative hypotheses and significance level (default to 0.05 if not given in a problem)
+        * Compute the test statistic $z_0$ i.e. the z score for the sample mean or proportion
+        * Compute the probability of having your test statistic ($Pr(z>z_0)$, $Pr(z<z_0)$, or $2Pr(z>|z_0|)$ depending
+        on the type of test.
+        """)
+
+    st.subheader("Method 3: Confidence Interval Approach (Only for 2 Tail)")
+
+    st.markdown(r"""
+        * Use the data you have to compute a confidence interval
+        * If the null hypothesis suggests the mean or proportion is outside your confidence interval, then
+        reject the null hypothesis
+        """)
+
+    st.markdown("I'd like to point out that you could use a confidence interval for 1 tail tests BUT we won't be "
+                "covering it in our course as we've only looked at symmetrical confidence intervals.")
